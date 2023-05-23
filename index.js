c3199ad66ea230d5b6f69d539ecd824cb19dc696
RICHARD-SIKAONGA <97953658+richie1988@users.noreply.github.com>
2023-05-23 03:20:45 +0200

    added javascript intaraction navbar

---
+++ index.js
@@ -0,0 +1,34 @@
+function setupMobileMenu() {
+    const hamburgerButton = document.getElementById('hamburger');
+    const closeMenuButton = document.getElementById('close-menu');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const menuOptions = document.querySelectorAll('#mobile-menu ul li a');
+    const sections = document.querySelectorAll('section');
+
+    function toggleMobileMenu() {
+        mobileMenu.classList.toggle('active');
+    }
+
+    function hideMobileMenu() {
+        mobileMenu.classList.remove('active');
+    }
+
+    function handleMenuOptionClick(e) {
+        hideMobileMenu();
+        var targetSectionId = e.target.getAttribute('href');
+        var targetSection = document.querySelector(targetSectionId);
+        sections.forEach(function(section) {
+            section.classList.remove('active');
+        });
+        targetSection.classList.add('active');
+    }
+
+    hamburgerButton.addEventListener('click', toggleMobileMenu);
+    closeMenuButton.addEventListener('click', hideMobileMenu);
+
+    menuOptions.forEach(function(option) {
+        option.addEventListener('click', handleMenuOptionClick);
+    });
+}
+
+setupMobileMenu();
