d25cd0325e33dffbcc8bcc60411bbe9979b83b6f
RICHARD-SIKAONGA <97953658+richie1988@users.noreply.github.com>
2023-05-23 19:43:04 +0200

    m

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 function setupMobileMenu() {
-    const hamburgerButton = document.getElementById('hamburger');
-    const closeMenuButton = document.getElementById('close-menu');
-    const mobileMenu = document.getElementById('mobile-menu');
-    const menuOptions = document.querySelectorAll('#mobile-menu ul li a');
-    const sections = document.querySelectorAll('section');
+    var hamburgerButton = document.getElementById('hamburger');
+    var closeMenuButton = document.getElementById('close-menu');
+    var mobileMenu = document.getElementById('mobile-menu');
+    var menuOptions = document.querySelectorAll('#mobile-menu ul li a');
+    var sections = document.querySelectorAll('section');
 
-    hamburgerButton.addEventListener('click', () => {
+    function toggleMobileMenu() {
         mobileMenu.classList.toggle('active');
-    });
+    }
 
-    closeMenuButton.addEventListener('click', () => {
+    function hideMobileMenu() {
         mobileMenu.classList.remove('active');
-    });
+    }
 
-    menuOptions.forEach(option => {
-        option.addEventListener('click', e => {
-            e.preventDefault();
-            mobileMenu.classList.remove('active');
-            const targetSectionId = e.target.getAttribute('href');
-            const targetSection = document.querySelector(targetSectionId);
-            sections.forEach(section => {
-                section.classList.remove('active');
-            });
-            targetSection.classList.add('active');
+    function handleMenuOptionClick(e) {
+        hideMobileMenu();
+        var targetSectionId = e.target.getAttribute('href');
+        var targetSection = document.querySelector(targetSectionId);
+        sections.forEach(function(section) {
+            section.classList.remove('active');
         });
+        targetSection.classList.add('active');
+    }
+
+    hamburgerButton.addEventListener('click', toggleMobileMenu);
+    closeMenuButton.addEventListener('click', hideMobileMenu);
+
+    menuOptions.forEach(function(option) {
+        option.addEventListener('click', handleMenuOptionClick);
     });
 }
 
