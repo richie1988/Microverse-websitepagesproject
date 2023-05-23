aec941048d7fa9a07ccd303b65156ac0a7eefa7f
RICHARD-SIKAONGA <97953658+richie1988@users.noreply.github.com>
2023-05-23 18:14:42 +0200

    modified linters

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function setupMobileMenu() {
     }
 
     hamburgerButton.addEventListener('click', toggleMobileMenu);
+    --
     closeMenuButton.addEventListener('click', hideMobileMenu);
 
     menuOptions.forEach(function(option) {
